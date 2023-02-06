997149cc15ff83fbc90123e0d233faac19d4514c
satyas469 <satyaprataps469@gmail.com>
2023-02-06 09:19:00 +0530

    my portfolio

---
+++ constants/data.js
@@ -0,0 +1,107 @@
+const userData = {
+  githubUsername: "satyendrasingh8",
+  name: "Satya Pratap Singh",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "satyaprataps469@gmail.com",
+  phone: "+91 7717457517",
+  address: "Prayagraj, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "Netflix",
+      link: "https://netflix-clone-5a6dc.web.app/",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Portfolio",
+      link: "/",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "chat application(ethereum)",
+      link: "/projects",
+      imgUrl: "/chatapp.png",
+    },
+    {
+      title: "Sports Bike",
+      link: "/",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "Flutter news application",
+      link: "/",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Flipkart Quiz Application",
+      link: "/",
+      imgUrl: "/feedmeback.png",
+    },
+
+    {
+      title: "3D HideAndSeek using reactVR and react360",
+      link: "/",
+      imgUrl: "/algochurn.png",
+    },
+
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 16 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express, MongoDB, MySQL/Postgres, Bootstrap, Antd,Tailwind, Sequelize.js, Adonisjs, Web3.js,Solidity ,Ethereum, Polygon,C, C++, Java, Python, Assembly, blogger, Youtuber etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: ".......",
+    currentProjectUrl: "/",
+  },
+  experience: [
+    {
+      title: "Full Stack Developer",
+      company: "PayPal",
+      year: "2022",
+      companyLink: "https://www.paypal.com/",
+      desc: "Contributed as a team member in frontend(HTML5,Css,Javascript,React .js etc) + backend(Node Js, Express, MongoDB,Sequelize.js,Adonis.js,Postgres etc) ",
+    },
+    {
+      title: "Javascript Fullstack Developer",
+      company: "Nagarro",
+      year: "2020",
+      companyLink: "https://www.nagarro.com/",
+      desc: "Worked as a team member in web development including frontend, backend, and database management systems.",
+    },
+    {
+      title: "Graduation",
+      company: "Indian Institute of Technology, Kharagpur",
+      year: "2018-22",
+      companyLink: "http://www.iitkgp.ac.in/",
+      desc: "Completed Bachelor of Technology from Indian Institute of Technology, Kharagpur with a cumulative grade point average of 8.2",
+    },
+    {
+      title: "Intermediate(10+2)",
+      company: "Maa Rajraani Inter College Prayagraj, Uttar Pradesh",
+      year: "2017",
+      companyLink: "/",
+      desc: "I had studied in one of the best colleges with the stream I wanted and scored 83 percentage in boards but I'm happy about it. ",
+    },
+    {
+      title: "High School",
+      company: "St. Vishna RR Nector school Mundera Prayagraj, Uttar Pradesh",
+      year: "2015",
+      companyLink: "https://www.facebook.com/St.vishna123/",
+      desc: "Subjects being PCM - I barely survived with 92% marks aggregate.Nobody asks this but it's okay.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1FXhTxkqYH0lt3tkf_jCMW5tiVaYz0sUt/view?usp=share_link",
+  socialLinks: {
+    linkedin: "/",
+    github: "/",
+    twitter: "/",
+    instagram: "/",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
